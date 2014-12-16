dc8e5cd3aa334ca15f61d26edc2010fb44ed4a8e
Apollo Tang <tang.apollo@gmail.com>
2014-12-16 12:34:26 -0500

    added note; start 1.4; added jshint inline conf to main.js

---
+++ e_firebase/1.4-structuring-data/app/scripts/controllers/main.js
@@ -0,0 +1,21 @@
+/* jshint undef: false, unused: false */
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 14StructuringDataApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 14StructuringDataApp
+ */
+angular.module('14StructuringDataApp')
+  .controller('MainCtrl', function ($scope) {
+    // $scope.awesomeThings = [
+    //   'HTML5 Boilerplate',
+    //   'AngularJS',
+    //   'Karma'
+    // ];
+
+
+
+  });
