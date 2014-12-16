6f80a5d74e1ac8f2d1fe7469554c4b33e11ce9ab
Apollo Tang <tang.apollo@gmail.com>
2014-12-16 00:39:42 -0500

    e_firebase:: video-1.3 6:13 - scarffolding with yo

---
+++ e_firebase/1.3-scarffolding-w-yo/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 13ScarffoldingWyoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 13ScarffoldingWyoApp
+ */
+angular.module('13ScarffoldingWyoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
