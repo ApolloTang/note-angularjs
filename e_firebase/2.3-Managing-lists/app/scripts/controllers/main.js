7a9802d0ccf2e02ab6dee17bc1d3c8a33385ebc8
Apollo Tang <tang.apollo@gmail.com>
2014-12-17 12:20:22 -0500

    e_firebase:: v2.5--1:50 : using onChild_changed

--- e_firebase/2.3-Managing-lists/app/scripts/controllers/main.js
+++ e_firebase/2.3-Managing-lists/app/scripts/controllers/main.js
@@ -18,23 +18,10 @@ angular.module('14StructuringDataApp')
     $scope.currentUser = null;
     $scope.currentText = null;
 
-
     // And becuse "child_added" return item one at a time you will
     // have to reconstruct the collection locally because
     // ng-repeat work with collection (which we will do next)
-
     $scope.messages = [];
-
-    messagesRef.on('value', function( snapshot ){
-        // with "onValue" every time item is added to collection,
-        // Firebase returns a snapshot object contain the entire
-        // collection of the reference
-        $timeout(function(){
-            var snapshotVal = snapshot.val();
-            // console.log('value: ', snapshotVal);
-            // $scope.messages = snapshotVal;
-        }, 0);
-    });
 
     messagesRef.on('child_added', function( snapshot ){
         // with "child_added", entire collection is fetched
@@ -52,9 +39,24 @@ angular.module('14StructuringDataApp')
         $timeout(function(){
             var snapshotVal = snapshot.val();
             console.log('** child_added: ', snapshotVal);
-            $scope.messages.push(snapshotVal);
-    }, 0);
-});
+            // $scope.messages.push(snapshotVal);
+            $scope.messages.push({
+                  text: snapshotVal.text
+                , user: snapshotVal.user
+                // , name: snapshot.name()  // <-- name() is deprecated
+                , name: snapshot.key()
+            });
+        }, 0);
+    });
+
+    messagesRef.on('child_changed', function( snapshot ){
+        $timeout(function(){
+            var snapshotVal = snapshot.val();
+            // console.log('&& child_changed: ', snapshot.name());  // <-- name() is deprecated
+            console.log('&& child_changed: ', snapshot.key());
+        }, 0);
+    });
+
 
     $scope.sendMessage = function(){
         var newMessage = {
