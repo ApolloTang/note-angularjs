7519d3f8bb874d8970c9d94acf8ac671400a318d
Apollo Tang <tang.apollo@gmail.com>
2014-12-17 11:59:34 -0500

    e_firebase:: v2.4--2:36 : onValue vs onChild_added

--- e_firebase/2.3-Managing-lists/app/scripts/controllers/main.js
+++ e_firebase/2.3-Managing-lists/app/scripts/controllers/main.js
@@ -20,10 +20,36 @@ angular.module('14StructuringDataApp')
 
 
     messagesRef.on('value', function( snapshot ){
+        // with "onValue" every time item is added to collection,
+        // Firebase returns a snapshot object contain the entire
+        // collection of the reference
         $timeout(function(){
             var snapshotVal = snapshot.val();
-            console.log(snapshotVal);
+            console.log('value: ', snapshotVal);
             $scope.messages = snapshotVal;
+        }, 0);
+    });
+
+    messagesRef.on('child_added', function( snapshot ){
+        // with "child_added", entire collection is fetched
+        // during the first "initialize". And during this first
+        // "initialize" collection items are received
+        // one item at a time (in onValue it is receive as a json
+        // with items nested within). After the initial fetch
+        // snapshot will only return newly added item.
+        //
+        // [!] Remember "on" event will fire on each browser
+        // that connects to firebase, if firebase return the
+        // entire collection when a single item is added
+        // you will get an expansive bill at the end of the month.
+        //
+        // And becuse "child_added" return item one at a time you will
+        // have to reconstruct the collection locally because
+        // ng-repeat work with collection (which we will do next)
+        $timeout(function(){
+            var snapshotVal = snapshot.val();
+            console.log('** child_added: ', snapshotVal);
+            // $scope.messages = snapshotVal;
         }, 0);
     });
 
