a89fd3a294a071b46560e11551eb92f1b9ab51b4
Apollo Tang <tang.apollo@gmail.com>
2014-12-17 20:08:46 -0500

    remove commented code

--- e_firebase/3.1/app/scripts/controllers/main.js
+++ e_firebase/3.1/app/scripts/controllers/main.js
@@ -14,19 +14,9 @@ angular.module('14StructuringDataApp')
   .controller('MainCtrl', function ($scope, $timeout, MessageService) {
 
         // var rootRef = new Firebase('https://fiery-fire-8387.firebaseio.com/');
-        // var messagesRef = rootRef.child('messages');
-        // var titleRef = rootRef.child('title');
 
-        // $scope.title = null;
     $scope.currentUser = null;
     $scope.currentText = null;
-
-        // titleRef.once('value', function(snapshot){
-        //     $timeout(function(){
-        //         $scope.title = snapshot.val();
-        //         console.log('value changed');
-        //     });
-        // });
 
     MessageService.childAdded(function(addedChild){
         console.log('MessageService.childAdded: ', addedChild);
@@ -37,45 +27,6 @@ angular.module('14StructuringDataApp')
 
     $scope.messages = [];
 
-        // messagesRef.on('child_changed', function( snapshot ){
-        //     $timeout(function(){
-        //         var snapshotVal = snapshot.val();
-        //         var message = findMessageByName(snapshot.key());
-        //         var newText = snapshotVal.text;
-        //         message.text = newText;
-        //     }, 0);
-        // });
-        //
-        // messagesRef.on('child_removed', function( snapshot ){
-        //     $timeout(function(){
-        //         deleteMessageByName(snapshot.key());
-        //     });
-        // });
-        //
-        // function deleteMessageByName(name) {
-        //     var i=0;
-        //     for (; i < $scope.messages.length; i++ ) {
-        //         var currentMessage = $scope.messages[i];
-        //         if (currentMessage.name === name) {
-        //             $scope.messages.splice(i,1);
-        //             break;
-        //         }
-        //     }
-        // }
-        //
-        // function findMessageByName(name) {
-        //     var messageFound = null;
-        //     var i=0;
-        //     for (; i < $scope.messages.length; i++ ) {
-        //         var currentMessage = $scope.messages[i];
-        //         if (currentMessage.name === name) {
-        //             messageFound = currentMessage;
-        //             break;
-        //         }
-        //     }
-        //     return messageFound;
-        // }
-
     $scope.sendMessage = function(){
         var newMessage = {
             user: $scope.currentUser
@@ -83,7 +34,6 @@ angular.module('14StructuringDataApp')
         };
         MessageService.add(newMessage);
     };
-
 
     $scope.turnFeedOff = function() {
         MessageService.off();
