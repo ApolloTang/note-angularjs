9d8cafa0c5cc8bee7679fefbcfa22e415eed5e06
Apollo Tang <tang.apollo@gmail.com>
2014-12-18 10:41:29 -0500

    e_firebase:: v3.3-done, using .startAt and .limit to give paging functionality

--- e_firebase/3.1/app/scripts/services/MessageService.js
+++ e_firebase/3.1/app/scripts/services/MessageService.js
@@ -11,9 +11,10 @@
         return {
             childAdded: function childAdded(limitNumber, callback){
                 messageRef
+                    // using .startAt and .limit to give paging functionality
                     .startAt( startAtPriority, '-JdPl-jf43PH9lk77Fg5' )
-                    .endAt( endAtPriority, '-JdT-EkL4w2WSvf2q_hT' )
-                    // .limit(limitNumber)   // only fetch the last 5
+                    // .endAt( endAtPriority, '-JdT-EkL4w2WSvf2q_hT' )
+                    .limit(limitNumber)
                     .on('child_added', function(snapshot){
                         var val = snapshot.val();
                         callback.call(this, { user: val.user, text: val.text, name: snapshot.key() } );
