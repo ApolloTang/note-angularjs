8514714622fb4aae7503a3fc9152ebd73d01d2bc
Apollo Tang <tang.apollo@gmail.com>
2014-12-18 10:39:53 -0500

    e_firebase:: v3.3-done, using FB.startAt() and FB.endEnd() instead of limit()

--- e_firebase/3.1/app/scripts/services/MessageService.js
+++ e_firebase/3.1/app/scripts/services/MessageService.js
@@ -6,10 +6,14 @@
     console.log('MessageService load');
     angular.module('14StructuringDataApp').service('MessageService', function(FBURL){
         var messageRef = new Firebase(FBURL).child('messages');
+        var startAtPriority = null;
+        var endAtPriority = null;
         return {
             childAdded: function childAdded(limitNumber, callback){
                 messageRef
-                    .limit(limitNumber)   // only fetch the last 5
+                    .startAt( startAtPriority, '-JdPl-jf43PH9lk77Fg5' )
+                    .endAt( endAtPriority, '-JdT-EkL4w2WSvf2q_hT' )
+                    // .limit(limitNumber)   // only fetch the last 5
                     .on('child_added', function(snapshot){
                         var val = snapshot.val();
                         callback.call(this, { user: val.user, text: val.text, name: snapshot.key() } );
